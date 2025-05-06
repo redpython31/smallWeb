3e6676872947603b53c55e70fe486b50166b9c8b
ReD PyThon <redpython31@gmail.com>
2025-05-07 02:37:33 +0300

    Add code in website.c for homepage

---
+++ website.c
@@ -0,0 +1,31 @@
+//Header Libraries------------
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <math.h>
+
+//Main function----------------
+
+    int main(){
+
+        int userInput; //Declare variable for home screen input.
+
+        while (1) {
+            printf("\n----------------PAGE START FROM HERE------------------\n");
+            printf("\nWelcome to Student Portal\n");
+
+            printf("\n\nPlease choose option by no. 1,2,3 :\n\n");
+            printf("1.Registraion\n");
+            printf("2.About Us\n");
+            printf("3.Contact Us\n");
+
+            if (scanf("%d",&userInput) != 1 )
+
+            
+
+        }
+
+        return 0;
+    }
